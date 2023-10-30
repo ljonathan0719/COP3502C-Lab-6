7c15d09fbddaba4aae814fe4d1d09fdc1dae5ac5
jonathandeng7 <jmdeng05@gmail.com>
2023-10-30 14:26:53 -0400

    added decode function and touched up the global

--- Lab 6.py
+++ Lab 6.py
@@ -5,9 +5,9 @@ menu_selection = ""
 def encoder(password):
     encoded_pass = ""
     for i in password:
-        value = int(i) - 3
-        if value < 0:  # cannot have value greater than 9 in any password
-            value += 10
+        value = int(i) + 3
+        if value > 10:  # cannot have value greater than 9 in any password
+            value -= 10
         encoded_pass += str(value)
     return encoded_pass
 
@@ -16,9 +16,7 @@ def menu():
     print("Menu")
     print("-------------")
     print("1. Encode\n2. Decode\n3. Quit")
-    menu_selection = input("Please enter an option: ")
 
-    return menu_selection
 
 #decoder function
 def decoder(encoded_pass):
@@ -37,11 +35,11 @@ if __name__ == "__main__":
     menu_selection = 0
     while menu_selection != 3:
         menu()
-        menu_selection = input("Please enter an option: ")
+        menu_selection = int(input("Please enter an option: "))
         if menu_selection == 1:
             password = input("Please enter your password to encode: ")
+            encoded_pass = encoder(password)
             print("Your password has been encoded and stored!")
         if menu_selection == 2:
-            encoded_pass = input("Please enter your encoded password: ")
             decoded_pass = decoder(encoded_pass)
             print(f"The encoded password is {encoded_pass}, and the original password is {decoded_pass}. ")
